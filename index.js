dcc21a3e68ce450ec754bb4a68b2e8beb23f5a8a
rasa thakkar <rasathakkar@gmail.com>
2021-08-25 11:59:43 +0530

    express done

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
-// console.log("Hello world");
+const path = require('path')
+const http = require('http')
+const chalk = require('chalk')
+const express = require('express')
+const app = express()
 
 
-// const add = (a,b) => {return a+b}
+app.use(express.urlencoded({extended:true}))
+const PORT = 8300
 
-// const print = (sum) => {console.log(sum)}
+// http.createServer((req,res) => {
+//     console.log(req.url);
+//     res.write('<html><h1>Welcome</h1></html>')
+//     res.end()
+//     console.log(chalk.yellow("Server Connected at PORT 8200"));
+//     // console.log(req);
+// }).listen(8200)
 
-// const sum = add(2,3)
-// console.log(sum);
+app.get('/',(req,res) => {
+    // console.log(req);
+    // console.log("At /");
+    res.sendFile(path.join(__dirname,'index.html'))
+    // res.json({msg:'Hello'})
+})
 
-console.log("Before Timeout");
+app.get('/contact',(req,res) => {
+    res.send('<h1>Contact US</h1>')
+})
 
-setTimeout(() => {
-    console.log("Inside Timeout");
-},3000)
+app.post('/addUser',(req,res) => {
+    console.log(req.body);
+    const email = req.body.email
+    const password = req.body.password
 
-console.log("After Timeout");
+    if(email == 'abcd' && password == '1234')
+    {
+        return res.redirect('/contact')
+    }
+    else {
+        return res.send('<h1>Login FAILED!!</h1>')
+    }
+})
+
+
+app.listen(PORT,(req,res) => {
+    console.log(`Connected at PORT ${PORT}`);
+})
